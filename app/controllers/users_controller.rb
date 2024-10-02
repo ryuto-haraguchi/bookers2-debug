42323b5c4db2b7db04bdfea8d89d16d91f1ab160
EC2 Default User <ec2-user@ip-172-31-9-73.ap-southeast-2.compute.internal>
2024-10-02 04:41:01 +0000

    [finish]フォロー、フォロワー機能実装ー応用課題４

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user! 
   before_action :ensure_correct_user, only: [:update, :edit]
+  before_action :set_user, only: [:show, :following, :followers]
 
   def show
     @user = User.find(params[:id])
@@ -24,6 +25,18 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
+  
+  def following
+    @title = "Follow Users"
+    @users = @user.following
+    render "show_follow"
+  end
+  
+  def followers
+    @title = "Follower Users"
+    @users = @user.followers
+    render "show_follow"
+  end
 
   private
 
@@ -36,6 +49,10 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to user_path(current_user)
     end
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
 end
