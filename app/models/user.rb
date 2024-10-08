41a579ff017a866ae4b1f1833a900f34feb56487
EC2 Default User <ec2-user@ip-172-31-9-73.ap-southeast-2.compute.internal>
2024-10-08 06:57:59 +0000

    [update]前日比

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,13 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
-  
+
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
-  
+
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
-  
+
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
   end
@@ -22,11 +22,11 @@ class User < ApplicationRecord
   def unfollow(other_user)
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
-  
+
   def following?(other_user)
     following.include?(other_user)
   end
-  
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
@@ -34,17 +34,30 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   def mutual_follow?(other_user)
     self.following?(other_user) && other_user.following?(self)
   end
 
   def todays_books_count
     books.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).count
-  end 
+  end
 
   def books_from_yesterday_count
     books.where(created_at: 1.day.ago.all_day).count
   end
-  
+
+  def boos_day_over_day_change
+    today_count = todays_books_count
+    
+    yesterday_count = books_from_yesterday_count
+    
+    return "No date for yesterday" if yesterday_count == 0
+    
+    change_ratio = ((today_count - yesterday_count).to_f / yesterday_count) * 100
+    
+    change_ratio.round(1)
+
+  end
+
 end
