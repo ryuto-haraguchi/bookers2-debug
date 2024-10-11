3b274fb203714ed4580d3b0187c832a366eda7cf
EC2 Default User <ec2-user@ip-172-31-9-73.ap-southeast-2.compute.internal>
2024-10-11 01:40:30 +0000

    [update]応用課題7-c finish

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,22 +2,31 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_group, only:[:show, :edit, :update]
   before_action :ensure_owner, only:[:edit, :update]
-  
-  
+
+
   def index
     @groups = Group.all
   end
 
   def show
   end
-  
+
   def edit
+  end
+
+  def update
+    if @group.updeta(group_params)
+      flash[:notice] = "Group was successfully updated."
+      redirect_to @group
+    else
+      render :edit
+    end
   end
 
   def new
     @group = Group.new
   end
-  
+
   def create
     @group = current_user.owned_groups.build(group_params)
     if @group.save
@@ -25,21 +34,21 @@ class GroupsController < ApplicationController
       redirect_to groups_path
     else
       render :new
-    end 
+    end
   end
-  
+
   private
-  
+
   def group_params
     params.require(:group).permit(:name, :introduction, :group_image)
-  end 
-  
-  def set_group 
+  end
+
+  def set_group
     @group = Group.find(params[:id])
   end
-  
+
   def ensure_owner
     redirect_to group_path, alert: "You are not authorized to edit this group." unless @group.owner == current_user
   end
-  
+
 end
